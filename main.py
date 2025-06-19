8ca5ebd4f119f3fbed171bb4a01849b4e29c9cc4
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-06-19 09:55:26 +0200

    opened a new window with a blank canvas

---
+++ main.py
@@ -0,0 +1,12 @@
+# Importing my own files
+from graphics import Window
+
+# Functions
+def main():
+    win = Window(800, 600)
+    win.wait_for_close()
+
+
+# Program starts here
+if __name__ == "__main__":
+    main()
