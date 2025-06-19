372e88606f14aa2764424d6ceb59547240d5bfb1
AlexVC97 <alex.vancoillie1297@gmail.com>
2025-06-19 10:16:04 +0200

    drew a line on the canvas

--- main.py
+++ main.py
@@ -1,9 +1,11 @@
 # Importing my own files
-from graphics import Window
+from graphics import Window, Point, Line
 
 # Functions
 def main():
     win = Window(800, 600)
+    l = Line(Point(50, 50), Point(400, 400))
+    win.draw_line(l, "black")
     win.wait_for_close()
 
 
